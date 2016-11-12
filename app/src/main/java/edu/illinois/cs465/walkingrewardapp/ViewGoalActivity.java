5a04705c270fb402dad4377713df7ef403f1b48d
Mengxiong <mliu60@illinois.edu>
2016-11-12 14:53:42 -0600

    finish view goal and view reward

--- app/src/main/java/edu/illinois/cs465/walkingrewardapp/ViewGoalActivity.java
+++ app/src/main/java/edu/illinois/cs465/walkingrewardapp/ViewGoalActivity.java
@@ -8,11 +8,16 @@ package edu.illinois.cs465.walkingrewardapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import edu.illinois.cs465.walkingrewardapp.Data.Challenge;
 
 public class ViewGoalActivity extends AppCompatActivity implements View.OnClickListener {
     private Button selectButton;
+    private Challenge goal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +30,18 @@ public class ViewGoalActivity extends AppCompatActivity implements View.OnClickL
             e.printStackTrace();
         }
 
+        goal = (Challenge) getIntent().getExtras().getSerializable("goal");
+        setTextView();
+
         selectButton = (Button) findViewById(R.id.view_goal_select);
         selectButton.setOnClickListener(this);
+    }
+
+    private void setTextView() {
+        TextView title = (TextView) findViewById(R.id.view_goal_title);
+        title.setText(goal.getTitle());
+        TextView description = (TextView) findViewById(R.id.view_goal_description);
+        description.setText(goal.getDescription());
     }
 
     @Override
