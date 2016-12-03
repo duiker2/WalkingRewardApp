004a4564611d48412b9ca1214e97b05b2c2da9d0
Mengxiong <mliu60@illinois.edu>
2016-12-03 16:24:27 -0600

    fix imports

--- app/src/main/java/edu/illinois/cs465/walkingrewardapp/WalkingActivity.java
+++ app/src/main/java/edu/illinois/cs465/walkingrewardapp/WalkingActivity.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -14,10 +13,10 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -26,8 +25,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -37,21 +36,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
-import static edu.illinois.cs465.walkingrewardapp.R.id.book_now;
-import static edu.illinois.cs465.walkingrewardapp.R.id.goal;
-import static java.lang.Double.min;
-import static java.lang.Double.valueOf;
-
-import android.widget.ProgressBar;
-
 import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import edu.illinois.cs465.walkingrewardapp.Maps.TouchableWrapper;
-
 import edu.illinois.cs465.walkingrewardapp.Data.Challenge;
-import edu.illinois.cs465.walkingrewardapp.Library;
+import edu.illinois.cs465.walkingrewardapp.Maps.TouchableWrapper;
 
 public class WalkingActivity extends AppCompatActivity implements
         GoogleMap.OnCameraMoveListener,
